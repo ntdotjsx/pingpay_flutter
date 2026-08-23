8bb2dce5797a2fb6900fb96d0b4123da48599585
ntdotjsx <0ms.ntdotjsx@gmail.com>
2026-08-24 05:58:22 +0700

    ios/Runner/RunnerRelease.entitlements

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,107 +1,13 @@
 import Flutter
-import FirebaseCore
-import FirebaseMessaging
 import UIKit
-import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate, MessagingDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // 1. Initialize Firebase Core if not already configured
-    if FirebaseApp.app() == nil {
-      FirebaseApp.configure()
-    }
-
-    // 2. Configure UNUserNotificationCenter delegate and request permission
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: { granted, error in
-          if let error = error {
-            print("[iOS FCM] Push Notification authorization error: \(error)")
-          } else {
-            print("[iOS FCM] Push Notification authorization granted: \(granted)")
-          }
-        }
-      )
-    }
-
-    // 3. Register for remote notifications on APNs
-    application.registerForRemoteNotifications()
-
-    // 4. Configure Messaging Delegate
-    Messaging.messaging().delegate = self
-
     GeneratedPluginRegistrant.register(with: self)
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // Handle APNs device token registration
-  override func application(
-    _ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  ) {
-    Messaging.messaging().apnsToken = deviceToken
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  }
-
-  override func application(
-    _ application: UIApplication,
-    didFailToRegisterForRemoteNotificationsWithError error: Error
-  ) {
-    print("[iOS FCM] Failed to register for remote notifications on APNs: \(error)")
-    super.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
-  }
-
-  // Required for background and data remote notifications
-  override func application(
-    _ application: UIApplication,
-    didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
-  ) {
-    Messaging.messaging().appDidReceiveMessage(userInfo)
-    completionHandler(.newData)
-  }
-
-  // MessagingDelegate callback when FCM registration token is updated
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-    if let token = fcmToken {
-      print("[iOS FCM] FCM Registration Token received: \(token)")
-    }
-  }
-
-  // Present notification banner when app is in foreground
-  override func userNotificationCenter(
-    _ center: UNUserNotificationCenter,
-    willPresent notification: UNNotification,
-    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-  ) {
-    let userInfo = notification.request.content.userInfo
-    Messaging.messaging().appDidReceiveMessage(userInfo)
-    if #available(iOS 14.0, *) {
-      completionHandler([.banner, .badge, .sound, .list])
-    } else {
-      completionHandler([.alert, .badge, .sound])
-    }
-  }
-
-  override func userNotificationCenter(
-    _ center: UNUserNotificationCenter,
-    didReceive response: UNNotificationResponse,
-    withCompletionHandler completionHandler: @escaping () -> Void
-  ) {
-    let userInfo = response.notification.request.content.userInfo
-    Messaging.messaging().appDidReceiveMessage(userInfo)
-    completionHandler()
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
